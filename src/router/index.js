9c769bf5bf87f0a15bb2032f0b463fc803c6a60f
Dane <darpera@yahoo.com>
2022-12-19 19:42:25 +0800

    Tekaaaa, may male

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
 import Equipments from '@/views/Equipments.vue'
-//import Rooms from '@/views/Rooms.vue'
+import Rooms from '@/views/Rooms.vue'
 
 const routes = [
   {
