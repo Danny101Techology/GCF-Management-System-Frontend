9067b5d1e09d3da4f8531932105dfbe107bbc8d1
Dane <darpera@yahoo.com>
2022-12-20 20:24:31 +0800

    Page Loader added UWU

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
  <q-layout view="hHh lpR fFf">
+    <PageLoader />
     <HeaderWow />
 
     <q-page-container>
@@ -10,11 +11,12 @@
 
 <script>
 import { ref, onMounted } from "vue";
-import { PanoViewer, SpinViewer } from "@egjs/vue3-view360";
 import axios from "axios";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faHatWizard } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import PageLoader from "@/components/PageLoader.vue";
 import HeaderWow from "@/components/HeaderWow.vue";
 
 
@@ -25,7 +27,7 @@ export default {
       autoplay: ref(true)
     }
   },
-  components: { HeaderWow }
+  components: { PageLoader, HeaderWow }
 
 
 
