b31e70e57c66ca0c9293e1d3fc96d78a7e9e6f55
Daniel Peralta <darpera@yahoo.com>
2022-12-30 08:21:40 +0800

    Cancellation

---
+++ src/views/Reservations.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="q-pa-md">
+      <RoomsTable :rooms="rooms" />
+    </div>
+  </template>
+  
+  <script setup>
+  import axios from "axios";
+  import { ref, onMounted } from "vue";
+  
+  import RoomsTable from "@/components/RoomsTable.vue";
+  
+  const rooms = ref([]);
+  
+  function retrieveFromAPI() {
+    axios.defaults.baseURL = process.env.VUE_APP_API_URI;
+    axios.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer ${process.env.VUE_APP_API_TOKEN}`;
+    axios
+      .get(`api/rooms`)
+      .then((response) => {
+        rooms.value = response.data.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  
+  onMounted(() => {
+    console.log("Rooms.vue have been mounted!");
+    retrieveFromAPI();
+  });
+  
+    
+  </script>
+  
