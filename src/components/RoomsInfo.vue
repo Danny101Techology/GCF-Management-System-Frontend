0de66e92d0cebb63bb30c1699bd8b4ceda1b0cf4
Dane <darpera@yahoo.com>
2023-03-22 09:11:26 +0800

    minor changeszzzz

--- src/components/RoomsInfo.vue
+++ src/components/RoomsInfo.vue
@@ -1,74 +1,43 @@
 <template>
-  <div class="q-pa-md">
-    <q-card class="my-card" flat bordered>
-      <q-item>
-        <q-item-section>
-          <q-item-label>{{ site }} > {{ name }}</q-item-label>
-          <q-item-label caption>Capacity: {{ capacity }} pax</q-item-label>
-        </q-item-section>
-      </q-item>
-
-      <q-separator />
-
-      <q-card-section vertical>
-        <div>
-          <q-linear-progress
-            v-if="!imageLoaded"
-            class="buffer-progress"
-            color="green"
-            indeterminate
-            rounded
-            track-color="white"
-            size="4px"
-            :min-height="2"
-          />
-          <q-carousel
-            animated
-            v-model="slide"
-            navigation
-            infinite
-            :autoplay="autoplay"
-            arrows
-            transition-prev="slide-right"
-            transition-next="slide-left"
-            @mouseenter="autoplay = false"
-            @mouseleave="autoplay = true"
-          >
-            <template v-for="img in images">
-              <q-carousel-slide
-                :name="img.name"
-                :img-src="img.url"
-                @load="imageLoaded = true"
-              />
-            </template>
-          </q-carousel>
-        </div>
-      </q-card-section>
-    </q-card>
-  </div>
+    <q-sticky position="top">
+      <div class="q-pa-md">
+        <template v-if="loading">
+          <!-- Skeleton loading effect -->
+          <q-skeleton class="my-card" height="300px" />
+        </template>
+        <template v-else>
+          <!-- Actual content -->
+          <q-card class="my-card" flat bordered>
+            <!-- ... card content ... -->
+            <q-carousel
+              animated
+              v-model="slide"
+              navigation
+              infinite
+              :autoplay="autoplay"
+              arrows
+              transition-prev="slide-right"
+              transition-next="slide-left"
+              @mouseenter="autoplay = false"
+              @mouseleave="autoplay = true"
+            >
+              <template v-for="img in images">
+                <q-carousel-slide :name="img.name" :img-src="img.url" />
+              </template>
+            </q-carousel>
+          </q-card>
+        </template>
+      </div>
+    </q-sticky>
 </template>
 
-<style lang="sass" scoped>
-.my-card
-  width: 100%
-  max-width: 950px
-
-.carousel-container
-  position: relative
-
-.buffer-progress
-  position: absolute
-  top: 50%
-  left: 50%
-  transform: translate(-50%, -50%)
-  z-index: 2
-</style>
 <script setup>
 import { ref, computed, onMounted } from "vue";
 
 const slide = ref(1);
 const autoplay = ref(true);
 const image = ref();
+const loading = ref(true);
 
 const reserve = ref(false);
 
@@ -85,5 +54,14 @@ const images = computed(() => props.room.images);
 onMounted(() => {
   console.log("RoomsDialog.vue have been mounted!");
   console.log(props.room);
+  setTimeout(() => {
+    loading.value = false;
+  }, 5000);
 });
 </script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 950px
+</style>
