ed14aedd9aba2bc1e22bfe6c95aeafd206fc307e
Dane <darpera@yahoo.com>
2023-03-26 19:52:46 +0800

    Logout buttonzzzz

--- src/components/HeaderWow.vue
+++ src/components/HeaderWow.vue
@@ -1,11 +1,10 @@
 <template>
   <q-header elevated class="bg-primary text-white" height-hint="98">
     <q-toolbar>
-
       <q-avatar>
         <img src="@/assets/logo.svg" />
       </q-avatar>
-
+      <q-btn flat round dense icon="logout" @click="logout" />
     </q-toolbar>
 
     <q-tabs align="left">
@@ -14,23 +13,30 @@
       <q-route-tab to="/reservation" label="Reservations" exact />
     </q-tabs>
   </q-header>
-
-
-
 </template>
 
 <script>
 import { ref } from "vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
 
 export default {
   setup() {
     const leftDrawerOpen = ref(false);
+    const store = useStore();
+    const router = useRouter();
+
+    function logout() {
+      store.dispatch("logout");
+      router.push("/login");
+    }
 
     return {
       leftDrawerOpen,
       toggleLeftDrawer() {
         leftDrawerOpen.value = !leftDrawerOpen.value;
       },
+      logout,
     };
   },
 };
