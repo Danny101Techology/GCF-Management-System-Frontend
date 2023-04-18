d6efc0544c829f6c32041b1959380f7ffe52ea41
Dane <darpera@yahoo.com>
2023-04-18 20:16:45 +0800

    added checkbox functionality (watch per ticked)

--- src/components/RoomsBookingCheckboxes.vue
+++ src/components/RoomsBookingCheckboxes.vue
@@ -29,7 +29,7 @@
       <div class="text-h6">Ticked</div>
       <div>
         <div v-for="tick in ticked" :key="`ticked-${tick}`">
-        {{ tick }}
+          {{ tick }}
         </div>
       </div>
       <!-- Food Arrangements Input
@@ -56,7 +56,7 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 
 // const showFoodArrangementInput = ref(false);
 
@@ -64,224 +64,476 @@ export default {
   setup() {
     const filter = ref("");
     const filterRef = ref(null);
+    const selected = ref([]);
+    const ticked = ref([]);
+    const expanded = ref([
+      "Things Needed",
+      "Physical Arrangements / Layout (with tables and chairs)",
+      "Food Arrangements",
+      "Cafeteria Supplies Needed",
+    ]);
+    const checkboxes = [
+      {
+        label: "Things Needed",
+        children: [
+          {
+            label: "A/V Equipment",
+            disabled: false,
+            children: [
+              { label: "Projector & Screen" },
+              { label: "Microphone & Speaker System" },
+              { label: "TV" },
+              { label: "Audio Player" },
+              { label: "Video Camera" },
+            ],
+          },
+          {
+            label: "Musical Instruments",
+            children: [
+              { label: "Acoustic/Bass Guitar" },
+              { label: "Keyboard" },
+              { label: "Piano" },
+              { label: "Drum Set" },
+              { label: "Other (*)" },
+            ],
+          },
+          {
+            label: "Furniture",
+            children: [
+              { label: "Lectern" },
+              { label: "White Board & Markers" },
+              { label: "Cross" },
+              { label: "Risers" },
+              { label: "Sofa Set" },
+            ],
+          },
+          {
+            label: "Sports Equipment",
+            children: [
+              { label: "Electronic Score Board & Timer" },
+              { label: "Badminton Net" },
+              { label: "Volleyball Net" },
+              { label: "Ping-pong Table & Surrounds" },
+              { label: "Gym Equipment" },
+            ],
+          },
+          {
+            label: "IT Equipment",
+            children: [
+              { label: "Wifi Access" },
+              { label: "Zoom Support" },
+              { label: "Hybrid set-up" },
+              { label: "Livestreaming" },
+            ],
+          },
+          {
+            label: "Maintenance Equipment",
+            children: [
+              { label: "Extension Cord" },
+              { label: "Aircon" },
+              { label: "Air-purifier" },
+              { label: "Iwata Fan" },
+              { label: "Ceiling fan & Exhaust fan" },
+            ],
+          },
+        ],
+      },
+      {
+        label: "Physical Arrangements / Layout (with tables and chairs)",
+        children: [
+          {
+            label: "Classroom Set-up (armchairs)",
+            disabled: false,
+          },
+          {
+            label: "Classroom Set-up (tables & chairs)",
+            disabled: false,
+          },
+          {
+            label: "Classroom Set-up (boardroom)",
+            disabled: false,
+          },
+          {
+            label: "Rectangular Table Set-up (20-50pax)",
+            disabled: false,
+          },
+          {
+            label: "Round Table Set-up (20-50pax)",
+            disabled: false,
+          },
+          {
+            label: "Round Table Set-up (50-100pax)",
+            disabled: false,
+          },
+          {
+            label: "Theater Type Set-up (20-50pax)",
+            disabled: false,
+          },
+          {
+            label: "Theater Type Set-up (50-100pax)",
+            disabled: false,
+          },
+          {
+            label: "Theater Type Set-up (>200pax)",
+            disabled: false,
+          },
+          {
+            label: "Registration Set-up",
+            disabled: false,
+          },
+        ],
+      },
+      {
+        label: "Food Arrangements",
+        children: [
+          {
+            label: "Packed Food",
+            disabled: false,
+          },
+          {
+            label: "Catered Buffet",
+            disabled: false,
+          },
+          {
+            label: "Pot-blessed",
+            disabled: false,
+          },
+        ],
+      },
+      {
+        label: "Cafeteria Supplies Needed",
+        children: [
+          {
+            label: "Brewed Coffee",
+            disabled: false,
+          },
+          {
+            label: "Creamer",
+            disabled: false,
+          },
+          {
+            label: "Sugar",
+            disabled: false,
+          },
+          {
+            label: "Coffee Stirrer",
+            disabled: false,
+          },
+          {
+            label: "Table Napkins",
+            disabled: false,
+          },
+          {
+            label: "Percolator",
+            disabled: false,
+          },
+          {
+            label: "Coffee Maker",
+            disabled: false,
+          },
+          {
+            label: "Paper Plates",
+            disabled: false,
+          },
+          {
+            label: "Paper Cups",
+            disabled: false,
+          },
+          {
+            label: "Plastic Spoons",
+            disabled: false,
+          },
+          {
+            label: "Plastic Fork",
+            disabled: false,
+          },
+          {
+            label: "Cutlery/Breakable Tablewares",
+            disabled: false,
+          },
+          {
+            label: "Water Dispenser",
+            disabled: false,
+          },
+        ],
+      },
+    ];
 
-    // const specialRequests = ref("");
-    // const foodArrangement = ref("");
+    watch(ticked, (newVal, oldVal) => {
+      // do something when ticked changes
+      console.log("ticked changed from", oldVal, "to", newVal);
+    });
+
+    const resetFilter = () => {
+      filter.value = "";
+      filterRef.value.focus();
+    };
+
+    const myfilterMethod = (node, filter) => {
+      // filter nodes based on their label
+      const label = node.label.toLowerCase();
+      return label.includes(filter.toLowerCase());
+    };
+
+    const renderNode = (node) => {
+      // render nodes with checkboxes
+      return <q-checkbox v-model={node.checked} label={node.label} />;
+    };
+
+    const updatedThingsNeeded = (newThingsNeeded) => {
+      ticked.value = newThingsNeeded;
+    };
 
     return {
       filter,
       filterRef,
-      // specialRequests,
-      // foodArrangement,
-      selected: ref([]),
-      ticked: ref([]),
-      expanded: ref([
-        "Things Needed",
-        "Physical Arrangements / Layout (with tables and chairs)",
-        "Food Arrangements",
-        "Cafeteria Supplies Needed",
-      ]),
-      checkboxes: [
-        {
-          label: "Things Needed",
-          children: [
-            {
-              label: "A/V Equipment",
-              disabled: false,
-              children: [
-                { label: "Projector & Screen" },
-                { label: "Microphone & Speaker System" },
-                { label: "TV" },
-                { label: "Audio Player" },
-                { label: "Video Camera" },
-              ],
-            },
-            {
-              label: "Musical Instruments",
-              children: [
-                { label: "Acoustic/Bass Guitar" },
-                { label: "Keyboard" },
-                { label: "Piano" },
-                { label: "Drum Set" },
-                { label: "Other (*)" },
-              ],
-            },
-            {
-              label: "Furniture",
-              children: [
-                { label: "Lectern" },
-                { label: "White Board & Markers" },
-                { label: "Cross" },
-                { label: "Risers" },
-                { label: "Sofa Set" },
-              ],
-            },
-            {
-              label: "Sports Equipment",
-              children: [
-                { label: "Electronic Score Board & Timer" },
-                { label: "Badminton Net" },
-                { label: "Volleyball Net" },
-                { label: "Ping-pong Table & Surrounds" },
-                { label: "Gym Equipment" },
-              ],
-            },
-            {
-              label: "IT Equipment",
-              children: [
-                { label: "Wifi Access" },
-                { label: "Zoom Support" },
-                { label: "Hybrid set-up" },
-                { label: "Livestreaming" },
-              ],
-            },
-            {
-              label: "Maintenance Equipment",
-              children: [
-                { label: "Extension Cord" },
-                { label: "Aircon" },
-                { label: "Air-purifier" },
-                { label: "Iwata Fan" },
-                { label: "Ceiling fan & Exhaust fan" },
-              ],
-            },
-          ],
-        },
-        {
-          label: "Physical Arrangements / Layout (with tables and chairs)",
-          children: [
-            {
-              label: "Classroom Set-up (armchairs)",
-              disabled: false,
-            },
-            {
-              label: "Classroom Set-up (tables & chairs)",
-              disabled: false,
-            },
-            {
-              label: "Classroom Set-up (boardroom)",
-              disabled: false,
-            },
-            {
-              label: "Rectangular Table Set-up (20-50pax)",
-              disabled: false,
-            },
-            {
-              label: "Round Table Set-up (20-50pax)",
-              disabled: false,
-            },
-            {
-              label: "Round Table Set-up (50-100pax)",
-              disabled: false,
-            },
-            {
-              label: "Theater Type Set-up (20-50pax)",
-              disabled: false,
-            },
-            {
-              label: "Theater Type Set-up (50-100pax)",
-              disabled: false,
-            },
-            {
-              label: "Theater Type Set-up (>200pax)",
-              disabled: false,
-            },
-            {
-              label: "Registration Set-up",
-              disabled: false,
-            },
-          ],
-        },
-        {
-          label: "Food Arrangements",
-          children: [
-            {
-              label: "Packed Food",
-              disabled: false,
-            },
-            {
-              label: "Catered Buffet",
-              disabled: false,
-            },
-            {
-              label: "Pot-blessed",
-              disabled: false,
-            },
-          ],
-        },
-        {
-          label: "Cafeteria Supplies Needed",
-          children: [
-            {
-              label: "Brewed Coffee",
-              disabled: false,
-            },
-            {
-              label: "Creamer",
-              disabled: false,
-            },
-            {
-              label: "Sugar",
-              disabled: false,
-            },
-            {
-              label: "Coffee Stirrer",
-              disabled: false,
-            },
-            {
-              label: "Table Napkins",
-              disabled: false,
-            },
-            {
-              label: "Percolator",
-              disabled: false,
-            },
-            {
-              label: "Coffee Maker",
-              disabled: false,
-            },
-            {
-              label: "Paper Plates",
-              disabled: false,
-            },
-            {
-              label: "Paper Cups",
-              disabled: false,
-            },
-            {
-              label: "Plastic Spoons",
-              disabled: false,
-            },
-            {
-              label: "Plastic Fork",
-              disabled: false,
-            },
-            {
-              label: "Cutlery/Breakable Tablewares",
-              disabled: false,
-            },
-            {
-              label: "Water Dispenser",
-              disabled: false,
-            },
-          ],
-        },
-      ],
+      selected,
+      ticked,
+      expanded,
+      checkboxes,
+      resetFilter,
+      myfilterMethod,
+      renderNode,
+      updatedThingsNeeded,
     };
   },
-
-  myFilterMethod(node, filter) {
-    const filt = filter.toLowerCase();
-    return (
-      node.label &&
-      node.label.toLowerCase().indexOf(filt) > -1 &&
-      node.label.toLowerCase().indexOf("(*)") > -1
-    );
-  },
-
-  resetFilter() {
-    filter.value = "";
-    filterRef.value.focus();
-  },
 };
+
+// export default {
+//   setup() {
+//     const filter = ref("");
+//     const filterRef = ref(null);
+//     const ticked = ref([]);
+
+//     const renderNode = (node) => {
+//       // render nodes with checkboxes
+//       return <q-checkbox v-model={node.checked} label={node.label} />;
+//     };
+
+//     const updatedThingsNeeded = (newThingsNeeded) => {
+//       ticked.value = newThingsNeeded;
+//     };
+
+//     watch(ticked, (newVal, oldVal) => {
+//       // do something when ticked changes
+//       console.log("ticked changed from", oldVal, "to", newVal);
+//     });
+
+//     return {
+//       filter,
+//       filterRef,
+//       renderNode,
+//       updatedThingsNeeded,
+//       selected: ref([]),
+//       ticked: ref([]),
+//       expanded: ref([
+//         "Things Needed",
+//         "Physical Arrangements / Layout (with tables and chairs)",
+//         "Food Arrangements",
+//         "Cafeteria Supplies Needed",
+//       ]),
+//       checkboxes: [
+//         {
+//           label: "Things Needed",
+//           children: [
+//             {
+//               label: "A/V Equipment",
+//               disabled: false,
+//               children: [
+//                 { label: "Projector & Screen" },
+//                 { label: "Microphone & Speaker System" },
+//                 { label: "TV" },
+//                 { label: "Audio Player" },
+//                 { label: "Video Camera" },
+//               ],
+//             },
+//             {
+//               label: "Musical Instruments",
+//               children: [
+//                 { label: "Acoustic/Bass Guitar" },
+//                 { label: "Keyboard" },
+//                 { label: "Piano" },
+//                 { label: "Drum Set" },
+//                 { label: "Other (*)" },
+//               ],
+//             },
+//             {
+//               label: "Furniture",
+//               children: [
+//                 { label: "Lectern" },
+//                 { label: "White Board & Markers" },
+//                 { label: "Cross" },
+//                 { label: "Risers" },
+//                 { label: "Sofa Set" },
+//               ],
+//             },
+//             {
+//               label: "Sports Equipment",
+//               children: [
+//                 { label: "Electronic Score Board & Timer" },
+//                 { label: "Badminton Net" },
+//                 { label: "Volleyball Net" },
+//                 { label: "Ping-pong Table & Surrounds" },
+//                 { label: "Gym Equipment" },
+//               ],
+//             },
+//             {
+//               label: "IT Equipment",
+//               children: [
+//                 { label: "Wifi Access" },
+//                 { label: "Zoom Support" },
+//                 { label: "Hybrid set-up" },
+//                 { label: "Livestreaming" },
+//               ],
+//             },
+//             {
+//               label: "Maintenance Equipment",
+//               children: [
+//                 { label: "Extension Cord" },
+//                 { label: "Aircon" },
+//                 { label: "Air-purifier" },
+//                 { label: "Iwata Fan" },
+//                 { label: "Ceiling fan & Exhaust fan" },
+//               ],
+//             },
+//           ],
+//         },
+//         {
+//           label: "Physical Arrangements / Layout (with tables and chairs)",
+//           children: [
+//             {
+//               label: "Classroom Set-up (armchairs)",
+//               disabled: false,
+//             },
+//             {
+//               label: "Classroom Set-up (tables & chairs)",
+//               disabled: false,
+//             },
+//             {
+//               label: "Classroom Set-up (boardroom)",
+//               disabled: false,
+//             },
+//             {
+//               label: "Rectangular Table Set-up (20-50pax)",
+//               disabled: false,
+//             },
+//             {
+//               label: "Round Table Set-up (20-50pax)",
+//               disabled: false,
+//             },
+//             {
+//               label: "Round Table Set-up (50-100pax)",
+//               disabled: false,
+//             },
+//             {
+//               label: "Theater Type Set-up (20-50pax)",
+//               disabled: false,
+//             },
+//             {
+//               label: "Theater Type Set-up (50-100pax)",
+//               disabled: false,
+//             },
+//             {
+//               label: "Theater Type Set-up (>200pax)",
+//               disabled: false,
+//             },
+//             {
+//               label: "Registration Set-up",
+//               disabled: false,
+//             },
+//           ],
+//         },
+//         {
+//           label: "Food Arrangements",
+//           children: [
+//             {
+//               label: "Packed Food",
+//               disabled: false,
+//             },
+//             {
+//               label: "Catered Buffet",
+//               disabled: false,
+//             },
+//             {
+//               label: "Pot-blessed",
+//               disabled: false,
+//             },
+//           ],
+//         },
+//         {
+//           label: "Cafeteria Supplies Needed",
+//           children: [
+//             {
+//               label: "Brewed Coffee",
+//               disabled: false,
+//             },
+//             {
+//               label: "Creamer",
+//               disabled: false,
+//             },
+//             {
+//               label: "Sugar",
+//               disabled: false,
+//             },
+//             {
+//               label: "Coffee Stirrer",
+//               disabled: false,
+//             },
+//             {
+//               label: "Table Napkins",
+//               disabled: false,
+//             },
+//             {
+//               label: "Percolator",
+//               disabled: false,
+//             },
+//             {
+//               label: "Coffee Maker",
+//               disabled: false,
+//             },
+//             {
+//               label: "Paper Plates",
+//               disabled: false,
+//             },
+//             {
+//               label: "Paper Cups",
+//               disabled: false,
+//             },
+//             {
+//               label: "Plastic Spoons",
+//               disabled: false,
+//             },
+//             {
+//               label: "Plastic Fork",
+//               disabled: false,
+//             },
+//             {
+//               label: "Cutlery/Breakable Tablewares",
+//               disabled: false,
+//             },
+//             {
+//               label: "Water Dispenser",
+//               disabled: false,
+//             },
+//           ],
+//         },
+//       ],
+//     };
+
+//   },
+
+//   myFilterMethod(node, filter) {
+//     const filt = filter.toLowerCase();
+//     return (
+//       node.label &&
+//       node.label.toLowerCase().indexOf(filt) > -1 &&
+//       node.label.toLowerCase().indexOf("(*)") > -1
+//     );
+//   },
+
+//   resetFilter() {
+//     filter.value = "";
+//     filterRef.value.focus();
+//   },
+// };
 </script>
